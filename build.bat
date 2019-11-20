0a6ca4c3ee7ff36ef9f16b6fa1455d9c3c8df9ab
Ryan Baxter <rbaxter@anyarinc.com>
2019-11-20 13:17:48 -0600

    UPDATE: config files for latest qt/msvc

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-::@echo off
-:: Req: CMAKE in path, QT_DIR set
+:: @echo off
+:: Req: CMAKE in path, MSVC installed, QT_DIR set
 
 set SQUARE=%CD%
 call "%SQUARE%\devops\scripts\config.bat"
 
-::set QT_DIR "C:\Qt\5.11.1\msvc2017_64\bin"
+:: set QT_DIR "C:\Qt\5.13.1\msvc2017_64\bin"
 
 call "%DEVOPS_SCRIPTS%\clean.bat"
 call "%DEVOPS_SCRIPTS%\update.bat"
