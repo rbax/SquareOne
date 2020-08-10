83c37abd09d33045195dc11017bce193afdb5367
ANYARINC\rbaxter <rbaxter@anyarinc.com>
2020-08-10 15:10:56 -0500

    UPDATE: qt build (adding platforms)

--- devops/scripts/update/qt.bat
+++ devops/scripts/update/qt.bat
@@ -12,35 +12,42 @@
 :: ---------------------------------------------------------- 
 
 IF NOT EXIST %DEPLOY_RELEASE% MKDIR %DEPLOY_RELEASE%
+IF NOT EXIST %DEPLOY_RELEASE% MKDIR %DEPLOY_RELEASE%/platforms
 IF NOT EXIST %DEPLOY_DEBUG% MKDIR %DEPLOY_DEBUG%
+IF NOT EXIST %DEPLOY_DEBUG% MKDIR %DEPLOY_DEBUG%/platforms
 
 :: ----------------------------------------------------------- [Core]
 
-xcopy /y /c /h "%QT_BIN%\Qt5Core.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Cored.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Core.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Cored.dll" %DEPLOY_DEBUG%
 
 :: ----------------------------------------------------------- [Gui]
 
-xcopy /y /c /h "%QT_BIN%\Qt5Gui.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Guid.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Gui.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Guid.dll" %DEPLOY_DEBUG%
 
 :: ----------------------------------------------------------- [Network]
 
-xcopy /y /c /h "%QT_BIN%\Qt5Network.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Networkd.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Network.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Networkd.dll" %DEPLOY_DEBUG%
 
 :: ----------------------------------------------------------- [QML]
 
-xcopy /y /c /h "%QT_BIN%\Qt5Qml.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Qmld.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Qml.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Qmld.dll" %DEPLOY_DEBUG%
 
-xcopy /y /c /h "%QT_BIN%\Qt5Quick.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Quickd.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Quick.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Quickd.dll" %DEPLOY_DEBUG%
 
-xcopy /y /c /h "%QT_BIN%\Qt5QuickWidgets.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5QuickWidgetsd.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5QuickWidgets.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5QuickWidgetsd.dll" %DEPLOY_DEBUG%
 
 :: ----------------------------------------------------------- [Widgets]
 
-xcopy /y /c /h "%QT_BIN%\Qt5Widgets.dll" %DEPLOY_RELEASE%
-xcopy /y /c /h "%QT_BIN%\Qt5Widgetsd.dll" %DEPLOY_DEBUG%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Widgets.dll" %DEPLOY_RELEASE%
+xcopy /f /y /c /h "%QT_BIN%\Qt5Widgetsd.dll" %DEPLOY_DEBUG%
+
+:: ----------------------------------------------------------- [Platform]
+
+xcopy /f /y /c /h "%QT_PLATFORM%\qwindows.dll" "%DEPLOY_RELEASE%\platforms\"
+xcopy /f /y /c /h "%QT_PLATFORM%\qwindowsd.dll" "%DEPLOY_DEBUG%\platforms\"
